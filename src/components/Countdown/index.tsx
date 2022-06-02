d6fc419a0f674cce4587c6a49c4c3b54ab158635
Pews-DEV <pedro.silvaifce@gmail.com>
2022-06-02 19:12:58 -0300

    feat: add buton to play countdown

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,21 +1,51 @@
+import { useEffect, useState } from "react";
+
 import * as S from "./styles";
 
 const Countdown = () => {
+  const [time, setTime] = useState(22 * 60);
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
+  const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
+
+  function startCountdown() {
+    setActive(!active);
+  }
+
+  useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [active, time]);
+
   return (
     <div>
       <S.CountdownContainer>
         <S.CountdownCouter>
-          <S.CountdownNubers>2</S.CountdownNubers>
-          <S.CountdownNubers>5</S.CountdownNubers>
+          <S.CountdownNubers>{minuteLeft}</S.CountdownNubers>
+          <S.CountdownNubers>{minuteRight}</S.CountdownNubers>
         </S.CountdownCouter>
         <S.CountdownTwoDots>:</S.CountdownTwoDots>
         <S.CountdownCouter>
-          <S.CountdownNubers>0</S.CountdownNubers>
-          <S.CountdownNubers>0</S.CountdownNubers>
+          <S.CountdownNubers>{secondLeft}</S.CountdownNubers>
+          <S.CountdownNubers>{secondRight}</S.CountdownNubers>
         </S.CountdownCouter>
       </S.CountdownContainer>
 
-      <S.CountdownButton type="button">Iniciar um ciclo</S.CountdownButton>
+      <S.CountdownButton
+        type="button"
+        onClick={() => {
+          startCountdown();
+        }}
+      >
+        Iniciar um ciclo
+      </S.CountdownButton>
     </div>
   );
 };
